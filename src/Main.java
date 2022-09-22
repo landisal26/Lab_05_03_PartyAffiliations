0f4b1507ef957ca66ab543e08e955ebda6930a3a
landisal <landisal@mail.uc.edu>
2022-09-22 13:36:01 -0400

    Finished

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
+        String party = "R";
+
+
+        System.out.println("What is your political affilitation? ");
+
+
+        if (party.equals("R"))
+        {
+            System.out.println("You get an Elephant");
+        }
+
+        if (party.equals("D"))
+        {
+            System.out.println("You get an Donkey");
+        }
+        if (party.equals("I"))
+        {
+            System.out.println("You Get the Human");
+        }
+        else
+        {
+            System.out.println("You selected other");
+        }
     }
 }
